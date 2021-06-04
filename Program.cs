6ef5465bc689cae9db65f821443544855f046c00
Gabriela <aragao.gabi@gmail.com>
2021-06-04 20:53:51 -0300

    Criação do vetor posicaoCliente

--- Program.cs
+++ Program.cs
@@ -6,14 +6,26 @@ namespace SBF_1
     class Program
     {
     static void Main(string[] args)
-        {
-            double x1, x2, y1, y2;
-            x1 = 12;
-            x2 = 13;
-            y1 = 11;
-            y2 = 10;
-            var distance = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
-            Console.WriteLine(distance);
+        {   
+            //int i;
+            Console.Write("x1: ");
+            int x1 = Convert.ToInt32(Console.ReadLine()); 
+
+            Console.Write("y1: ");    
+            int y1 = Convert.ToInt32(Console.ReadLine()); 
+                
+            int[] posicaoCliente = new int[2] {x1,y1};
+
+            
+            Console.WriteLine("{0},{1}" ,x1,y1);
+            Console.WriteLine("{0},{1}", posicaoCliente[0], posicaoCliente[1]);
+            
+
+            
+
+            
+
+           
         }
     }
 }
