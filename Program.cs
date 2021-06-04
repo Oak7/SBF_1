bc1efdadd1c7d7ef702406d8abc29658707b58d8
Gabriela <aragao.gabi@gmail.com>
2021-06-04 19:27:39 -0300

    cálculo da distância entre 2 pontos

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 
+
 namespace SBF_1
 {
     class Program
     {
-        static void Main(string[] args)
+    static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double x1, x2, y1, y2;
+            x1 = 12;
+            x2 = 13;
+            y1 = 11;
+            y2 = 10;
+            var distance = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
+            Console.WriteLine(distance);
         }
     }
 }
