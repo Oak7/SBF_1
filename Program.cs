f7483bbb39d9e3dad748276f56f34f80f42809cc
Gabriela <aragao.gabi@gmail.com>
2021-06-04 22:11:23 -0300

    novo Cálculo da distância entre dois pontos

--- Program.cs
+++ Program.cs
@@ -15,17 +15,11 @@ namespace SBF_1
             Console.Write("y1: ");    
             int y1 = Convert.ToInt32(Console.ReadLine()); 
                 
-            int[] posicaoCliente = new int[2] {x1,y1};
-            
-            Array.Sort(posicaoCliente); //ordenação dos vetores
-    
-            foreach(int item in posicaoCliente)
-            {
-                Console.WriteLine(item);
-            }
-            
-            
+            //int[,] posicaoCliente = new int[x1, y1];
+            int[] posicaoCliente = new int[2] {x1, y1};
 
+                       
+         
             /*******  Vetor Plano  ***************/
 
             Console.Write("M: ");
@@ -34,18 +28,27 @@ namespace SBF_1
             Console.Write("N: ");    
             int N = Convert.ToInt32(Console.ReadLine()); 
                 
-            int[] plano = new int[2] {M,N};
-                        
-    
-            foreach(int item in plano)
-            {
-                Console.WriteLine(item);
-            }
-            Array.Sort(plano);
+            //int[,] plano = new int[M,N];
+            int[] plano = new int[2] {M, N};  
 
+                     
             
+            /********matriz lojas ****************/
+            Console.Write("x2: ");
+            int x2 = Convert.ToInt32(Console.ReadLine()); 
 
-           
+            Console.Write("y2: ");    
+            int y2 = Convert.ToInt32(Console.ReadLine()); 
+
+            //int[,] lojas = new int[x2,y2];
+            int[] lojas = new int[2] {x2, y2};
+
+           /**********Cálculo da distência entre cliente e lojas ****************/
+
+           var distance = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
+          // Array.Sort();
+          Console.WriteLine(distance);
+
         }
     }
 }
